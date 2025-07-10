062f2616e52d71b7b1894e2fbdd907ab3e1f89bf
Lucas <lucaselanobasso@gmail.com>
2025-07-09 21:59:13 -0300

    Adicionando fixtures para gerir os corpos das requisições

--- test/transferencia.test.js
+++ test/transferencia.test.js
@@ -2,6 +2,7 @@ const request = require('supertest');
 const  { expect } = require('chai')
 require('dotenv').config()
 const { obterToken } = require('../helpers/autenticacao.js')
+const postTransferencias = require('../fixtures/postTransferencias.json')
 
 describe('Transferências', () => {
     describe('POST/transferencias', () => {
@@ -10,32 +11,24 @@ describe('Transferências', () => {
             token = await obterToken('julio.lima', '123456')
         })
         it('Deve retornar sucesso com 201 quando o valor da transferência for igual ou acima de R$10,00', async () =>{
+            const bodyTransferencias = { ...postTransferencias}
             const resposta = await request(process.env.BASE_URL)
                 .post('/transferencias')
                 .set('Content-Type', 'application/json')
                 .set('Authorization', `Bearer ${token}`)
-                .send({
-                    contaOrigem: 1,
-                    contaDestino: 2,
-                    valor: 11,
-                    token: ""
-                    })
+                .send(bodyTransferencias)
                 expect(resposta.status).to.equal(201);
             
         })  
 
-        it('Deve retornar falha 422 quando o valor da transferência for abaixo que R$10,00', async () =>{
-            
+        it('Deve retornar falha 422 quando o valor da transferência for abaixo que R$10,00', async () =>{ 
+            const bodyTransferencias = { ...postTransferencias}
+            bodyTransferencias.valor = 7
             const resposta = await request(process.env.BASE_URL)
                 .post('/transferencias')
                 .set('Content-Type', 'application/json')
                 .set('Authorization', `Bearer ${token}`)
-                .send({
-                    contaOrigem: 1,
-                    contaDestino: 2,
-                    valor: 7,
-                    token: ""
-                    })
+                .send(bodyTransferencias)
                 expect(resposta.status).to.equal(422);
             
         })
